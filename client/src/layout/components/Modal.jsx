ef785701eb841164e3e22e85b67ce4119ab158d0
Aman <aman@trukt.com>
2026-02-16 15:07:39 -0800

    removing scrollbar in list

--- client/src/layout/components/Modal.jsx
+++ client/src/layout/components/Modal.jsx
@@ -24,7 +24,7 @@ export const Modal = ({ children }) => {
 
       <div className={`
         fixed inset-y-0 left-0 z-30 w-72 bg-gray-100 dark:bg-gray-800 border-r dark:border-gray-700 overflow-y-auto
-        transform transition-transform duration-300 ease-in-out
+        transform transition-transform duration-300 ease-in-out no-scrollbar 
         ${isOpen ? 'translate-x-0' : '-translate-x-full'}
         md:relative md:translate-x-0 md:z-0
       `}>
